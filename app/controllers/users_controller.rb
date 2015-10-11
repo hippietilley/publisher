c9bb3ddf0c0e73842657082cdd996bfdb449a327
Shane Becker <veganstraightedge@gmail.com>
2015-10-11 09:29:25 -0400

    Add user auth. [closes #1]
    Routes for auth: /signup, /signin, /signout.
    Also, to edit a password or email address: /settings
    
    Based on: http://railscasts.com/episodes/270-authentication-in-rails-3-1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,66 @@
+class UsersController < ApplicationController
+  before_action :set_user,      only: [:show, :update, :destroy]
+  before_filter :authorize,     only: [:edit, :update, :destroy]
+
+
+  # /signup
+  def new
+    @user = User.new
+  end
+
+  # /signup
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
+  end
+
+
+  # GET /users
+  # TODO: ADMIN ONLY
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # TODO: hide id in URL, use @username
+  def show
+  end
+
+  # /settings
+  def edit
+    @user  = current_user
+  end
+
+  # /settings
+  def update
+    @user = current_user
+    if current_user.update(user_params)
+      redirect_to root_url, notice: "Your settings were successfully updated."
+    else
+      render action: "edit"
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    redirect_to root_url, notice: "User was successfully destroyed."
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email,
+                                   :password,
+                                   :password_confirmation)
+    end
+end
