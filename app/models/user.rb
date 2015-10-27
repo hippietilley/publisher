c38e28a378da78a64436a628fddfc586f315d0a1
Shane Becker <veganstraightedge@gmail.com>
2015-10-27 17:38:06 -0400

    Add post types association to user [#76]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: :true, on: :create
   validates :email,    presence: :true, uniqueness: true, on: [:create, :update]
+
+  has_many :post_types
 end
