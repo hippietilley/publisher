80b2595c9f09ab20d65b790a8a917914f0dc5edf
Shane Becker <veganstraightedge@gmail.com>
2015-11-06 13:06:52 -0500

    Add normalize, pagination and alerts (from Bootstrap) css [updates #96]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030055712) do
+ActiveRecord::Schema.define(version: 20151105230953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20151030055712) do
     t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "test"
   end
 
   create_table "users", force: :cascade do |t|
