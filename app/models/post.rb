6163744b1e6d8a8266f2536a26dbedf3ee9b1776
Shane Becker <veganstraightedge@gmail.com>
2015-11-19 17:49:21 -0500

    Fix tags retrieval on /new forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,7 @@ class Post < ActiveRecord::Base
 
   def tags
     output = []
-    Tagging.where(post_type: post_type_type.downcase, post_id: post_type_id).all.find_each do |tagging|
+    Tagging.where(post_type: post_type_type.try(:downcase), post_id: post_type_id).all.find_each do |tagging|
       output << Tag.find(tagging.tag_id)
     end
     output
