a8e3272cdcba7deca787ea117653f86f000acaba
Shane Becker <veganstraightedge@gmail.com>
2015-11-21 18:37:21 -0500

    Don't include Pages in homepage feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
     @slug = "home"
-    @posts = Post.all.paginate(page: params[:page] || 1)
+    @posts = Post.where.not(post_type_type: "Page").all.paginate(page: params[:page] || 1)
   end
 end
