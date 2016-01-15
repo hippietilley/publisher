11bb95704394dd7f4fbeada19dec8f46180b93ff
Shane Becker <veganstraightedge@gmail.com>
2016-01-14 18:44:54 -0800

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111213941) do
+ActiveRecord::Schema.define(version: 20160113185759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,7 +141,6 @@ ActiveRecord::Schema.define(version: 20160111213941) do
     t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "test"
   end
 
   create_table "users", force: :cascade do |t|
