d7124476096a00210bd0f525b8fcc37a3f2d244e
Shane Becker <veganstraightedge@gmail.com>
2016-01-15 16:12:55 -0800

    Add links to external tag pages from /tags/* [closes #131]

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -16,4 +16,21 @@ module TagsHelper
   def link_to_tag(tag)
     link_to_unless_current(tag.name, tag_path(tag.slug), class: "p-category", rel: "tag")
   end
+
+  def link_to_external_tag_pages(tag)
+    links = []
+
+    {
+      Facebook: "https://www.facebook.com/hashtag/",
+      Flickr: "https://www.flickr.com/photos/tags/",
+      Instagram: "https://www.instagram.com/explore/tags/",
+      Twitter: "https://twitter.com/hashtag/",
+      Vimeo: "https://vimeo.com/categories/",
+      Vine: "https://vine.co/tags/",
+    }.each do |site, url|
+      links << link_to(site, url + tag.slug)
+    end
+
+    links.join(", ").html_safe
+  end
 end
