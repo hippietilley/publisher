4c1ac5f10e5570befa951881b889b58e965bafa0
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-28 14:54:19 -0800

    Align stuff in syndication controller spec

--- spec/controllers/syndicators_controller_spec.rb
+++ spec/controllers/syndicators_controller_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe SyndicatorsController, type: :controller do
   fixtures :users
   let!(:user) { users(:user) }
+
   before do
     allow(controller).to receive(:current_user) { user }
   end
+
   describe "POST 'create'" do
     fixtures :posts, :providers
-    let(:note) { posts(:note) }
-    let(:provider) { providers(:twitter) }
+    let(:note)           { posts(:note) }
+    let(:provider)       { providers(:twitter) }
     let(:twitter_client) { double("Twitter::Rest::Client") }
+
     it "uses the twitter gem to syndicate to twitter" do
       user.providers << provider
       note.update(content: "I'm content")
