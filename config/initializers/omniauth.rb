6a85dcc4e18aeea3ba553e5b95396b74b406c50a
Bookis Smuin <bookis.smuin@gmail.com>
2016-02-04 16:08:44 -0800

    Fix broken things (Settings table not existing, drop not happening) during bin/setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  if Setting.of(:syndication_twitter_secret)
-    provider :twitter,
-      Setting.of(:syndication_twitter_key).content,
-      Setting.of(:syndication_twitter_secret).content
+  if ActiveRecord::Base.connection.table_exists? 'settings'
+    if Setting.of(:syndication_twitter_secret)
+      provider :twitter,
+        Setting.of(:syndication_twitter_key).content,
+        Setting.of(:syndication_twitter_secret).content
+    end
   end
 end
