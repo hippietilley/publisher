c33a980aa44a805148621e15b6b94a4038a2660f
Shane Becker <veganstraightedge@gmail.com>
2016-03-31 10:52:32 -0700

    Guard /new behind signin [closes #172]

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
+  before_action :authorize
+  before_action :set_on_admin_page
+
   def new
     @page_title = "Dashboard"
     @slug = "dashboard"
