3f265c8476e65cfb3d211cea27d8d7999288f71e
Shane Becker <veganstraightedge@gmail.com>
2016-05-04 19:58:22 -0700

    Add posttype/form switcher to /new [closes #225 #224]

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,11 +5,21 @@ class DashboardController < ApplicationController
 
   def new
     @page_title = "NEW Post"
-    @slug = "new"
+    @slug       = "new"
+
+    @activity = PostForm.new(Activity)
+    @article  = PostForm.new(Article)
+    @bookmark = PostForm.new(Bookmark)
+    @event    = PostForm.new(Event)
+    @note     = PostForm.new(Note)
+    @page     = PostForm.new(Page)
+    @photo    = PostForm.new(Photo)
+    @sound    = PostForm.new(Sound)
+    @video    = PostForm.new(Video)
   end
 
   def index
     @page_title = "Dashboard"
-    @slug = "dashboard"
+    @slug       = "dashboard"
   end
 end
